61bf0020ef7d8b9ae56532fbb3701c6b52cc7455
Philippe Mouchel <philippe@symetris.ca>
2019-02-13 12:28:26 -0500

    Modal events OK.

---
+++ js/modals-handler.js
@@ -0,0 +1,20 @@
+$(function() {
+
+    $('.modal').each(function() {
+      var modalIframe = $(this).find('iframe');
+
+      // Keep Iframe URL in mind.
+      $(this).data('iframe-src', modalIframe.attr('src'));
+
+      // Fill iframe src when opening.
+      $(this).on('show.bs.modal', function (e) {
+        modalIframe.attr('src', $(this).data('iframe-src'));
+      });
+
+      // Empty it when closing.
+      $(this).on('hide.bs.modal', function (e) {
+        modalIframe.attr('src', '#');
+    });
+  });
+
+});
